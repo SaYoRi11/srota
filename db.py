9019e7a3d6dfba063e9f370892e346bd10a62bf5
Suraj Giri <gsuraj1111@gmail.com>
2022-05-22 18:33:02 +0545

    First commit

---
+++ db.py
@@ -0,0 +1,26 @@
+import os, sqlalchemy, urllib, databases
+
+host_server = os.environ.get('HOST_SERVER')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('DB_SERVER_PORT')))
+database_name = os.environ.get('DATABASE_NAME')
+db_username = urllib.parse.quote_plus(str(os.environ.get('POSTGRES_USER')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('POSTGRES_PASSWORD')))
+ssl_mode = urllib.parse.quote_plus(str(os.environ.get('SSL_MODE')))
+DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("email", sqlalchemy.String),
+    sqlalchemy.Column("password", sqlalchemy.String)
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, pool_size=3, max_overflow=0
+)
+metadata.create_all(engine)
---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, status, Depends
+from fastapi_login import LoginManager
+from fastapi.middleware.cors import CORSMiddleware
+from db import database, users
+from pydantic import BaseModel
+from fastapi_login.exceptions import InvalidCredentialsException
+from fastapi.security import OAuth2PasswordRequestForm
+import os
+
+app = FastAPI()
+manager = LoginManager(str(os.environ.get('SECRET_KEY')), token_url='/auth/token')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class User(BaseModel):
+    email: str
+
+class UserDB(User):
+    password: str
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+@manager.user_loader()
+async def load_user(email: str):
+    query = users.select().where(users.c.email == email)
+    return await database.fetch_one(query)
+
+@app.post('/auth/token')
+async def login(data: OAuth2PasswordRequestForm = Depends()):
+    email = data.username
+    password = data.password
+
+    user = await load_user(email)
+    if not user:
+        raise InvalidCredentialsException
+    elif password != user['password']:
+        raise InvalidCredentialsException
+    
+    access_token = manager.create_access_token(
+        data=dict(sub=email)
+    )
+    return {'access_token': access_token, 'token_type': 'bearer'}
+
+@app.post('/register', response_model=User, status_code = status.HTTP_201_CREATED)
+async def register(user: UserDB):
+    query = users.insert().values(email=user.email, password=user.password)
+    last_user_id = await database.execute(query)
+    return {**user.dict(), "id": last_user_id}
+
+
+@app.get("/")
+async def read_root(user = Depends(manager)):
+    return {"Hello": "World"}
